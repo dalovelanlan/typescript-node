b6c4023e40b228989bc7c0f8dcc0adb47b7666b1
dalovelanlan <15138027270@163.com>
2023-03-04 15:05:33 +0800

    完成工具开发

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,35 @@
 // import routers from './router'
-import requestPage from './router/requestPage'
+import { time, timeEnd } from "console";
+import requestPage from "./router/requestPage";
 
-requestPage.requestPage('https://www.amazon.co.uk/dp/B07BFW53WH?th=1')
+const main = async () => {
+  // 这里只是一个 pb 的使用示例，不包含任何功能
+
+  //   const url = "https://www.amazon.co.uk/dp/B07BFW53WH";
+  const url = `https://www.amazon.co.uk/dp/B07BFW53WH
+      https://www.amazon.co.uk/dp/B07RTXY8RB
+      https://www.amazon.co.uk/dp/B07RQLPYPV
+      https://www.amazon.co.uk/dp/B07RRVYGJW
+      https://www.amazon.co.uk/dp/B07TYNQ1DG
+      https://www.amazon.co.uk/dp/B07RXRBRCD
+      https://www.amazon.co.uk/dp/B07TWM75R4
+      https://www.amazon.co.uk/dp/B07TVJC5KR
+      https://www.amazon.co.uk/dp/B07VBY3KSV`;
+  const urlData = url.split(/\s+/);
+
+  const array = [];
+  var num = 0,
+    total = urlData.length;
+  console.log("开始执行.......");
+  time("执行时间");
+  for (const url of urlData) {
+    num++;
+    const data = await requestPage(url, num, total);
+    const newData = data.replace(" out of 5 stars", "");
+    array.push({ url, data: newData });
+  }
+  timeEnd("执行时间");
+  console.log(array);
+};
+main();
+// requestPage.requestPage('https://www.amazon.co.uk/dp/B07BFW53WH?th=1')
--- src/router/requestPage.ts
+++ src/router/requestPage.ts
@@ -1,43 +1,48 @@
-const request = require('request');
-const cheerio = require('cheerio');
-
-// 
-
+const request = require("request");
+const cheerio = require("cheerio");
+import ProgressBar from "../utils/ProgressBar";
 
 var header = {
-  'Connection': 'keep-alive',
-  'content-type': 'application/json',
-  'Accept': '*/*',
-  'Sec-Fetch-Site': 'cross-site',
-  'Sec-Fetch-Mode': 'cors',
-  'Sec-Fetch-Dest': 'empty',
- 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1 wechatdevtools/1.05.2204250 MicroMessenger/8.0.5 Language/zh_CN webview/',
-}
-
-
-// node http request https://www.amazon.co.uk/dp/B07BFW53WH?th=1
-const requestPage = (page: string) => {
-  request({
-    url: page,
-    method: 'GET',
-    headers: header,
-  }, (error: any, response: any, body: any) => {
-    if(error) {
-      console.log(error);
-    }     
-    const $ = cheerio.load(body);
-    const container = $('body > #a-page > #dp > .a-container > span[data-hook="rating-out-of-text"]').html();
-
-    console.log('container',container);
-    
-    // data.each((index: any, element: any) => {
-    //   console.log('element',element);
-    // })
-    
-  })
-}
-
-
-export default {
-  requestPage,
+  Connection: "keep-alive",
+  "content-type": "application/json",
+  Accept: "*/*",
+  "Sec-Fetch-Site": "cross-site",
+  "Sec-Fetch-Mode": "cors",
+  "Sec-Fetch-Dest": "empty",
+  "User-Agent":
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1 wechatdevtools/1.05.2204250 MicroMessenger/8.0.5 Language/zh_CN webview/",
 };
+
+// node http request
+const requestPage = (page: string, number?: number, total?: number) => {
+  const pb = new ProgressBar("执行进度", 50);
+  return new Promise<string>((resolve, reject) => {
+    request(
+      {
+        url: page,
+        method: "GET",
+        headers: header,
+      },
+      (error: any, response: any, body: any) => {
+        if (error) {
+          console.log(error);
+        }
+        const $ = cheerio.load(body);
+        const data = $(
+          "body > #a-page > #dp > .a-container > #ppd > #productTitleGroupAnchor > #titleExpander >#productTitleExpanderRow >.a-fixed-right-grid-inner >#averageCustomerReviews_feature_div   > #acrCustomerReviewLink > i > span"
+        ).html();
+        if (number <= total) {
+          // 更新进度条
+          pb.render({ completed: number, total: total });
+        }
+        if (data) {
+          resolve(data);
+        } else {
+          resolve("0");
+        }
+      }
+    );
+  });
+};
+
+export default requestPage;
