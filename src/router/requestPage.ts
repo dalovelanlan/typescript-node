a11a97f60b2280828a7478590a2abb0cd6f5cfa5
dalovelanlan <15138027270@163.com>
2023-03-18 11:28:26 +0800

    添加注释

--- src/router/requestPage.ts
+++ src/router/requestPage.ts
@@ -2,6 +2,7 @@ const request = require("request");
 const cheerio = require("cheerio");
 import ProgressBar from "../utils/ProgressBar";
 
+// 配置头部
 var header = {
   Connection: "keep-alive",
   "content-type": "application/json",
